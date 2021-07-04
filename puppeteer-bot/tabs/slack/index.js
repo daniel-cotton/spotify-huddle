38e5bd907c2b2d838b785096c369c26bee7c8582
Dan Cotton <dan@daniel-cotton.co.uk>
2021-07-04 01:54:22 +0100

    fix(slack): fix slack user data

--- puppeteer-bot/tabs/slack/index.js
+++ puppeteer-bot/tabs/slack/index.js
@@ -4,7 +4,8 @@ module.exports = new (class SlackTab {
     async open() {
         this.browser = await puppeteer.launch({
             headless: false,
-            executablePath: process.env.EXECUTABLE
+            executablePath: process.env.EXECUTABLE,
+            userDataDir: '.data/slack'
         });
         this.page = await this.browser.newPage();
         await this.page.goto(process.env.SLACK_CHANNEL_URL);            
