4199e2c917646df9bb4c5ba8b7c841652e430b0e
Dan Cotton <dan@daniel-cotton.co.uk>
2021-07-04 14:59:44 +0100

    fix(playback): attempt play and queue in one step to avoid error

--- slackbot/handlers/queueing/index.js
+++ slackbot/handlers/queueing/index.js
@@ -25,8 +25,9 @@ module.exports = (boltApp, expressRouter, spotifyConnectionManager) => {
         if (command.text) {
             // Acknowledge command request
             await ack();
-            const spotifyClient = spotifyConnectionManager.getClient();
-            if (spotifyClient) {
+            try {
+                const spotifyClient = spotifyConnectionManager.getClient();
+                if (spotifyClient) {
                     const data = await spotifyClient.searchTracks(command.text);
                     const tracks = data.body.tracks.items;
         
@@ -38,7 +39,34 @@ module.exports = (boltApp, expressRouter, spotifyConnectionManager) => {
                         blocks,
                         text: `Added ${track.name} to the queue.`
                     });
-
+    
+                }
+            } catch (e) {
+                try {
+                    const spotifyClient = spotifyConnectionManager.getClient();
+                    if (spotifyClient) {
+                        const data = await spotifyClient.searchTracks(command.text);
+                        const tracks = data.body.tracks.items;
+            
+                        const track = tracks[0];
+            
+                        await spotifyClient.play({
+                            device_id: spotifyConnectionManager.deviceID,
+                            uris: [track.uri]
+                        })
+    
+                        const blocks = TrackQueuedBlockBuilder(track);
+                        await say({
+                            blocks,
+                            text: `Added ${track.name} to the queue.`
+                        });
+        
+                    }
+                } catch (e) {
+                    await say({
+                        text: `Something went wrong, please retry later.`
+                    });
+                }
             }
         }
     });
