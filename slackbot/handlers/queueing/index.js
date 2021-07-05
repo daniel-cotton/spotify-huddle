6cccffbaee45e3d62f4f3fb4a36f302fb48bcaa9
Dan Cotton <dan@daniel-cotton.co.uk>
2021-07-05 19:34:28 +0100

    fix(queueing): add play/queue logic to search too

--- slackbot/handlers/queueing/index.js
+++ slackbot/handlers/queueing/index.js
@@ -1,28 +1,62 @@
 const TrackQueuedBlockBuilder = require('../../lib/slack/blocks/TrackQueuedBlockBuilder.js');
 
 module.exports = (boltApp, expressRouter, spotifyConnectionManager) => {
+
+    const enqueueTrack = (track, user) => {
+        
+        try {
+            const spotifyClient = spotifyConnectionManager.getClient();
+            const queue = spotifyConnectionManager.getQueue();
+            if (spotifyClient) {
+                await spotifyClient.addToQueue(track.uri);
+                queue.enqueue(track, user);
+                return track;
+            }
+        } catch (e) {
+            try {
+                const spotifyClient = spotifyConnectionManager.getClient();
+                const queue = spotifyConnectionManager.getQueue();
+                if (spotifyClient) {
+                    await spotifyClient.play({
+                        device_id: spotifyConnectionManager.deviceID,
+                        uris: [track.uri]
+                    })
+                    queue.enqueue(track, user);
+
+                    return track;
+                }
+            } catch (e) {
+                console.error(e);
+                return null;
+            }
+        }
+    }
     
     boltApp.action('queue_track', async ({ action, ack, client, body }) => {
         // Acknowledge command request
         await ack();
         const { value } = action;
         const spotifyClient = spotifyConnectionManager.getClient();
-        const queue = spotifyConnectionManager.getQueue();
         const user = body.user ? body.user : {
             username: body.user_name,
         };
         if (spotifyClient) {
             const track = await spotifyClient.getTrack(value.split(":").pop());
-            await spotifyClient.addToQueue(value);
-            queue.enqueue(track, user);
-
-            const blocks = TrackQueuedBlockBuilder(track.body, user);
-            client.chat.update({
-                channel: body.channel.id,
-                ts: body.message.ts,
-                blocks,
-                text: `Added ${track.body.name} to the queue.`
-            });
+            
+            const enqueuedTrack = enqueueTrack(track.body, user);
+            if (enqueuedTrack) {
+                const blocks = TrackQueuedBlockBuilder(track.body, user);
+                client.chat.update({
+                    channel: body.channel.id,
+                    ts: body.message.ts,
+                    blocks,
+                    text: `Added ${track.body.name} to the queue.`
+                });
+            } else {
+                await say({
+                    text: `Something went wrong, please retry later.`
+                });
+            }
         }
     });
 
@@ -30,56 +64,35 @@ module.exports = (boltApp, expressRouter, spotifyConnectionManager) => {
         if (command.text) {
             // Acknowledge command request
             await ack();
+
             const user = {
-                username: body.user_name,
+                username: body.user_name
             };
+
             try {
                 const spotifyClient = spotifyConnectionManager.getClient();
-                const queue = spotifyConnectionManager.getQueue();
                 if (spotifyClient) {
                     const data = await spotifyClient.searchTracks(command.text);
                     const tracks = data.body.tracks.items;
         
                     const track = tracks[0];
-                    await spotifyClient.addToQueue(track.uri);
-                    queue.enqueue(track, user);
-        
-                    const blocks = TrackQueuedBlockBuilder(track, user);
-                    await say({
-                        blocks,
-                        text: `Added ${track.name} to the queue.`
-                    });
-    
-                }
-            } catch (e) {
-                try {
-                    const spotifyClient = spotifyConnectionManager.getClient();
-                    const queue = spotifyConnectionManager.getQueue();
-                    if (spotifyClient) {
-                        const data = await spotifyClient.searchTracks(command.text);
-                        const tracks = data.body.tracks.items;
-            
-                        const track = tracks[0];
-            
-                        await spotifyClient.play({
-                            device_id: spotifyConnectionManager.deviceID,
-                            uris: [track.uri]
-                        })
-                        queue.enqueue(track, user);
-    
+                    const enqueuedTrack = enqueueTrack(track, user);
+                    if (enqueuedTrack) {
                         const blocks = TrackQueuedBlockBuilder(track, user);
                         await say({
                             blocks,
                             text: `Added ${track.name} to the queue.`
                         });
-        
+                    } else {
+                        await say({
+                            text: `Something went wrong, please retry later.`
+                        });
                     }
-                } catch (e) {
-                    console.error(e);
-                    await say({
-                        text: `Something went wrong, please retry later.`
-                    });
                 }
+            } catch (e) {
+                await say({
+                    text: `I couldn't find any tracks with that name, maybe try another?`
+                });
             }
         }
     });
